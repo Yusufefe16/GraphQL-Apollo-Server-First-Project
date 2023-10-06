b9d7c2afae2679944424fe44dbf483e9140fea1e
Yusufefe16 <90358311+Yusufefe16@users.noreply.github.com>
2023-10-06 23:36:00 +0300

    Mutation

--- index.js
+++ index.js
@@ -51,6 +51,120 @@ const resolvers = {
     event(parent) {
       return data.events.find((e)=>e.id === parseInt(parent.event_id));
     }
+  },
+  Mutation: {
+    deleteEvents(_,args) {
+      data.events = data.events.filter((e)=> e.id !== parseInt(args.id));
+      return data.events;
+    },
+    deleteLocations(_,args) {
+      data.locations = data.locations.filter((e)=> e.id !== parseInt(args.id));
+      return data.locations;
+    },
+    deleteParticipants(_,args) {
+      data.participants = data.participants.filter((e)=> e.id !== parseInt(args.id));
+      return data.participants;
+    },
+    deleteUsers(_,args) {
+      data.users = data.users.filter((e)=> e.id !== parseInt(args.id));
+      return data.users;
+    },
+    addEvent(_,args) {
+      let game = {
+        ...args.event,
+        id: Math.floor(Math.random()*10000)
+      }
+      data.events.push(game);
+      return game;
+    },
+    addLocations(_,args) {
+      let location = {
+        ...args.location,
+        id: Math.floor(Math.random()*10000)
+      }
+      data.locations.push(location);
+      return location;
+    },
+    addParticipants(_,args) {
+      let participant = {
+        ...args.participant,
+        id: Math.floor(Math.random()*10000)
+      }
+      data.participants.push(participant);
+      return participant;
+    },
+    addUsers(_,args) {
+      let user = {
+        ...args.user,
+        id: Math.floor(Math.random()*10000)
+      }
+      data.users.push(user);
+      return user;
+    },
+    updateEvent(_,args) {
+      data.events = data.events.map((e)=>{
+        if (e.id === parseInt(args.id)) {
+          return {...e, ...args.edit}
+        }
+        return e
+      })
+      return data.events.find((e)=> e.id === parseInt(args.id));
+    },
+    updateLocations(_,args) {
+      data.locations = data.locations.map((e)=>{
+        if (e.id === parseInt(args.id)) {
+          return {...e, ...args.edit}
+        }
+        return e
+      })
+      return data.locations.find((e)=> e.id === parseInt(args.id));
+    },
+    updateParticipants(_,args) {
+      data.participants = data.participants.map((e)=>{
+        if (e.id === parseInt(args.id)) {
+          return {...e, ...args.edit}
+        }
+        return e
+      })
+      return data.participants.find((e)=> e.id === parseInt(args.id));
+    },
+    updateUser(_,args) {
+      data.users = data.users.map((e)=>{
+        if (e.id === parseInt(args.id)) {
+          return {...e, ...args.edit}
+        }
+        return e
+      })
+      return data.users.find((e)=> e.id === parseInt(args.id));
+    },
+    deleteAllEvents: () =>{
+      const length = data.events.length;
+      data.events.splice(0,length);
+      return{
+        count: length,
+      };
+    },
+    deleteAllLocations: () =>{
+      const length = data.locations.length;
+      data.locations.splice(0,length);
+      return{
+        count: length,
+      };
+    },
+    deleteAllParticipants: () =>{
+      const length = data.participants.length;
+      data.participants.splice(0,length);
+      return{
+        count: length,
+      };
+    },
+    deleteAllUsers: () =>{
+      const length = data.users.length;
+      data.users.splice(0,length);
+      return{
+        count: length,
+      };
+    }
   }
 };
 //server setup
--- sceme.js
+++ sceme.js
@@ -13,12 +13,46 @@ export const typeDefs = `#graphql
         user: User!
     }
 
+    input AddEventInput {
+        title: String!
+        desc: String!
+        date: String!
+        from: String!
+        to: String!
+        location_id: ID! 
+        user_id: ID!
+    }
+
+    input EditEventInput{
+        title: String
+        desc: String
+        date: String
+        from: String
+        to: String
+        location_id: ID
+        user_id: ID
+    }
+
     type Location{
         id: ID!
         name: String!
         desc: String!
         lat: Float!
         lng: Float!
+    }
+
+    input AddLocationInput {
+        name: String!
+        desc: String!
+        lat: Float!
+        lng: Float!
+    }
+
+    input EditLocationsInput {
+        name: String
+        desc: String
+        lat: Float
+        lng: Float
     }
 
     type Participant{
@@ -29,10 +63,34 @@ export const typeDefs = `#graphql
         event: Event!
     }
 
+    input AddParticipantInput {
+        user_id: ID!
+        event_id: ID!
+    }
+
+    input EditParticipantsInput {
+        user_id: ID
+        event_id: ID
+    }
+
     type User{
         id: ID!
         username: String!
         email: String!
+    }
+    
+    input AddUserInput {
+        username: String!
+        email: String!
+    }
+
+    input EditUserInput {
+        username: String
+        email: String
+    }
+
+    type DeleteAllOutput {
+        count: Int!
     }
 
     type Query {
@@ -44,6 +102,25 @@ export const typeDefs = `#graphql
         participant(id:ID!):Participant
         users: [User]
         user(id: ID!): User
+    }
+
+    type Mutation {
+        addEvent(event: AddEventInput!): Event
+        addLocations(location: AddLocationInput!): Location
+        addParticipants(participant: AddParticipantInput!): Participant
+        addUsers(user: AddUserInput!): User
+        deleteEvents(id:ID!): [Event]
+        deleteLocations(id:ID!): [Location] 
+        deleteParticipants(id:ID!): [Participant]
+        deleteUsers(id:ID!): [User]
+        updateEvent(id:ID!, edit: EditEventInput!): Event
+        updateLocations(id:ID!, edit: EditLocationsInput!): Location
+        updateParticipants(id:ID!, edit: EditParticipantsInput!): Participant
+        updateUser(id:ID!, edit: EditUserInput!): User
+        deleteAllEvents: DeleteAllOutput!
+        deleteAllLocations: DeleteAllOutput!
+        deleteAllParticipants: DeleteAllOutput!
+        deleteAllUsers: DeleteAllOutput!
     }
 
 `;
